b70a0afa64920e402b2b1dcf86b71b5123cef41a
Anuj Gupta <azg0076@auburn.edu>
2018-02-01 21:53:40 -0600

    Reading FASTA sequences and scoring matrix.

---
+++ project1.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python -uB
+import re
+import sys
+
+def main(sequenceFiles):
+    seq1 = readSequence(sequenceFiles[0])
+    seq2 = readSequence(sequenceFiles[1])
+
+    mat = readMatrix('BLOSUM62.txt')
+    print mat
+
+def readSequence(fileName):
+    fileStream = open(fileName, 'r')
+    seq = fileStream.read().decode('utf-8')
+    seq = ''.join(seq.split('\n')[1:])
+    return seq
+
+def readMatrix(fileName):
+    fileStream = open(fileName, 'r')
+    strMatrix = fileStream.read().decode('utf-8')
+
+    lines = strMatrix.split('\n')
+    header = re.sub(' +', ' ', lines[0].strip()).split(' ')
+    header = re.sub('\*', ' ', '|'.join(header)).split('|')
+
+    outDict = dict()
+    for line in lines[1:]:
+        cols = re.sub(' +', ' ', line.strip()).split(' ')
+        if cols[0] == '*':
+            cols[0] = ' '
+        outDict[cols[0]] = dict(zip(header, [int(col) for col in cols[1:]]))
+    return outDict
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print >> sys.stderr, "Usage: {0} FASTA_sequence1 FASTA_sequence2".format(sys.argv[0])
+        sys.exit()
+    else:
+        main(sys.argv[1:3])
