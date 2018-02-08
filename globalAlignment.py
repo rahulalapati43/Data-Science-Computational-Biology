25cda7b963dac70869a83c0e82320389e0a03898
Vineet Nayak <vineetnayak28@gmail.com>
2018-02-07 23:59:40 -0600

    Modified trace but dptable still giving wrong values

--- globalAlignment.py
+++ globalAlignment.py
@@ -6,18 +6,22 @@ def globalAlignment(sequence1, sequence2, scoringMatrix):
           
    # Step 1: Initialize Matrix 
     dpTable = constructDPTable(sequence1, sequence2, scoringMatrix)
-    traceback(scoringMatrix, sequence1, sequence2)
+    traceback(dpTable, sequence1, sequence2)
     
 def constructDPTable(sequence1, sequence2, matrix):
     dpTable = [[dict([('score', 0)]) for i in range(len(sequence2)+1)] for j in range(len(sequence1)+1)] 
    
     # Step 2: Base cases
-    # consuming letters from seq1 and aligning against space
+    # consuming letters from sequence1 and aligning against space
     for j in range(1, len(sequence1)+1):
         dpTable[0][j]['score'] = dpTable[0][j-1]['score'] + matrix[sequence1[j-1]][' ']
-    # consuming letters from seq2 and aligning against space
+        dpTable[0][j]['parentI'] = 0
+        dpTable[0][j]['parentJ'] = j-1
+    # consuming letters from sequence2 and aligning against space
     for i in range(1, len(sequence2)+1):
         dpTable[i][0]['score'] = dpTable[i-1][0]['score'] + matrix[' '][sequence2[i-1]]
+        dpTable[i][0]['parentI'] = i-1
+        dpTable[0][j]['parentJ'] = 0
         
     #Step 3: Fill the DP table    
     for i in range(1, len(sequence2)+1):
@@ -41,54 +45,71 @@ def constructDPTable(sequence1, sequence2, matrix):
     return dpTable
 
 
-def traceback(matrix, seq1, seq2):
-    j = len(seq1) 
-    i = len(seq2) 
+def traceback(dpTable, sequence1, sequence2):
+    j = len(sequence1)
+    i = len(sequence2)
+    print dpTable
     S1 = []
     S2 = []
     S3 = []
-    while i > 0 and j > 0:
+    while i > 0 and j > 0:       
+        # print dpTable[i][j]['score']
+        # print dpTable[i-1][j-1]['score']
         #Diagonal
-        val = -1
-        if(seq1[j-1] == seq2[i-1]):
-            val = 1
+        if(i-1 == dpTable[i][j]['parentI'] and j-1 == dpTable[i][j]['parentJ']):
             S3.append('|')
-        
-            
-        if int(matrix[i][j]) == int(matrix[i-1][j-1]) + int(val): 
-            S2.append(seq2[i-1])
-            S1.append(seq1[j-1])
-            if(val == -1):
-                S3.append('*')
-            i -= 1
-            j -= 1
-        
+            S1.append(sequence1[i-1])
+            S2.append(sequence2[j-1])
         #Left
-        elif matrix[i][j] == matrix[i-1][j] + space: 
-            S2.append(seq2[i-1])
-            S1.append('_')
-            S3.append('*')
-            i -= 1
-            
-        #Right
-        elif matrix[i][j] == matrix[i][j-1] + space: 
+        elif (i == dpTable[i][j]['parentI'] and j-1 == dpTable[i][j]['parentJ']):
             S2.append('_')
-            S1.append(seq1[j-1])
+            S1.append(sequence1[j-1])
             S3.append('*')
-            j -= 1
+        #Right
+        elif (i-1 == dpTable[i][j]['parentI'] and j == dpTable[i][j]['parentJ']): 
+            S2.append(sequence2[i-1])
+            S1.append('_')
+            S3.append('*') 
+        i= dpTable[i][j]['parentI']
+        j= dpTable[i][j]['parentJ']
+
+
+    #     print int(dpTable[i][j]['score'])
+    #     print int(dpTable[i-1][j-1]['score'])
+    #     if int(dpTable[i][j]['score']) == int(dpTable[i-1][j-1]['score']) + int(val): 
+    #         S2.append(sequence2[i-1])
+    #         S1.append(sequence1[j-1])
+    #         if(val == -1):
+    #             S3.append('*')
+    #         i -= 1
+    #         j -= 1
+        
+    #     Left
+    #     elif i>0 and dpTable[i][j]['score'] == dpTable[i-1][j]['score'] + space: 
+    #         S2.append(sequence2[i-1])
+    #         S1.append('_')
+    #         S3.append('*')
+    #         i -= 1
             
-        #When sequences are unequal    
-    while i > 0: 
-       S2.append(seq2[i-1])
-       S1.append('_')
-       S3.append('*')
-       i -= 1
+    #     #Right
+    #     elif j>0 and dpTable[i][j]['score'] == dpTable[i][j-1]['score'] + space: 
+    #         S2.append('_')
+    #         S1.append(sequence1[j-1])
+    #         S3.append('*')
+    #         j -= 1
+            
+    #     #When sequences are unequal    
+    # while i > 0: 
+    #    S2.append(sequence2[i-1])
+    #    S1.append('_')
+    #    S3.append('*')
+    #    i -= 1
        
-    while j > 0:
-        S2.append('_')
-        S1.append(seq1[j-1])
-        S3.append('*')
-        j -= 1
+    # while j > 0:
+    #     S2.append('_')
+    #     S1.append(sequence1[j-1])
+    #     S3.append('*')
+    #     j -= 1
         
     print(S1[::-1])
     print(S3[::-1])    
