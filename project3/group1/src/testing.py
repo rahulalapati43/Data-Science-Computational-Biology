69d0a1d8fde3a3c22e10e98d5d65bd4996a0188e
Anuj Gupta <azg0076@auburn.edu>
2018-03-26 11:05:45 -0500

    Fixing prediction code

--- project3/group1/src/testing.py
+++ project3/group1/src/testing.py
@@ -3,21 +3,33 @@ from util import *
 import sys
 import cPickle
 import math
-def main(gaussianModel, faFile):
+def main(gaussianModel, pssm_file, ss_file):
 	# read model
 	gaussianModel = cPickle.load(open(gaussianModel, 'rb'))	
-	#read faFile
-	proteinSequences = readData(faFile)
-	testHeader, test20Features = readPSSM("test.pssm")
+
+	testHeader, test20Features = readPSSM(pssm_file)
 	testFeatures = slidingWindow(test20Features)
-	
-	result = predict(gaussianModel,testFeatures)
-	print result
+
+	actualShapes = ''.join(readData(ss_file))
+	predictions = predict(gaussianModel,testFeatures)
+
+	successfulPredictions = {'H': 0, 'E': 0, 'C': 0}
+	for pssmKey, prediction in predictions.iteritems():
+		aminoAcid = prediction[0]
+		shapePrediction = prediction[1]
+		actual = actualShapes[int(pssmKey) - 1]
+		if actual == shapePrediction:
+			successfulPredictions[actual] += 1
+
+	print 'Total Q3 Accuracy: ' + str(sum(successfulPredictions.values()) / float(len(actualShapes)))
+	print 'H Q3 Accuray:' + str(successfulPredictions['H'] / float(actualShapes.count('H')))
+	print 'E Q3 Accuray:' + str(successfulPredictions['E'] / float(actualShapes.count('E')))
+	print 'C Q3 Accuray:' + str(successfulPredictions['C'] / float(actualShapes.count('C')))
 
 
-def pdf(x,mean,sd):
-	fra = 1/ math.sqrt(2* math.pi * (sd**2))
-	exp = math.exp(-0.5 * (((x - mean)/sd)**2))
+def pdf(x, mean, variance):
+	fra = 1/ math.sqrt(2 * math.pi * variance)
+	exp = math.exp(-0.5 / variance * (((x - mean)) ** 2))
 	pdf = fra * exp
 	return pdf
 
@@ -49,8 +61,8 @@ def predict(gaussianModel,testFeatures):
 	return result
 
 if __name__ == "__main__":
-        if len(sys.argv) < 3:
-                print "Usage: " + sys.argv[0] + "gaussianModel.pickle FASTA_file"
+        if len(sys.argv) < 4:
+                print "Usage: " + sys.argv[0] + " gaussianModel.pickle pssm_file ss_file"
         else:
-                main(sys.argv[1], sys.argv[2])
+                main(sys.argv[1], sys.argv[2], sys.argv[3])
  
