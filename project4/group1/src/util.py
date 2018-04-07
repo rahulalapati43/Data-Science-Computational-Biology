b2152d8bd11700ca748cd436eb073e9dc32904d4
Grand251 <austinrm66@gmail.com>
2018-04-06 20:27:56 -0500

    Update util.py
    Added both getSubsets, as well as a bi-functional predict function. These have been added to utils so that they may be used in training as well as validation / new data.

--- project4/group1/src/util.py
+++ project4/group1/src/util.py
@@ -1,6 +1,7 @@
 import random
 import os
 import subprocess
+import math
 
 def decodeFastaformat(fastaStream):
     output = list()
@@ -25,6 +26,31 @@ def randomSplit(inList, rate):
     trainData = inList[:(int((len(inList)) * rate))]
     testData = inList[(int((len(inList)) * rate)):]
     return trainData, testData
+
+def getSubsetBatch(dataset):
+    return dataset
+
+def getSubsetStochastic(dataset):
+    return random.choice(dataset)
+
+def predict(instance, weights): #adjusts with or without bias
+    i = 0 #iterator for instance
+    j = 0 #iterator for weights
+    temp = 0
+    
+    if (len(instance) + 1) == len(weights): #weights contain bias
+        temp += weights[j] #bias
+        j++
+        
+    while (i < len(instance)):
+        temp += instance[i] * weights[j]
+        i++
+        j++
+    temp = -temp
+    temp = 1 + math.exp(temp)
+    return = 1 / temp
+    
+    
 
 if __name__ == "__main__":
     pass
